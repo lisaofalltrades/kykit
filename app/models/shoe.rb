ccce4b91d6b1cb7b935defab353227ee703b92b9
Lisa Nguyen <email": null>
2017-01-17 16:51:01 -0800

    Adding shoe association

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,3 +1,6 @@
 class Shoe < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :seller, :class_name => "User"
+  has_many :sales, :class_name => "Purchase"
+  has_many :purchasers, through => :sales, :source => :purchaser
 end
