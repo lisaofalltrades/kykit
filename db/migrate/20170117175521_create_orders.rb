f155ba2d9e9e99eb56b510f273a76d020d4e4d42
Lisa Nguyen <email": null>
2017-01-17 18:09:19 -0800

    setting up orders table

--- db/migrate/20170117175521_create_orders.rb
+++ db/migrate/20170117175521_create_orders.rb
@@ -1,4 +1,11 @@
 class CreateOrders < ActiveRecord::Migration
   def change
+    create_table :orders do |t|
+      t.integer :purchaser_id
+      t.datetime :order_date #set default to today in form
+      t.decimal :grand_total
+      t.string :shipping_address
+
+      t.timestamp
   end
 end
