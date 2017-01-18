663038cb08fab1aa89ee509d5227db906384739c
Lisa Nguyen <email": null>
2017-01-17 17:12:32 -0800

    saving CreateUsers table migration DRAFT

--- db/migrate/20170117165448_create_users.rb
+++ db/migrate/20170117165448_create_users.rb
@@ -1,4 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :hashed_password, null: false
+
+      t.timestamp
+    end
   end
 end
