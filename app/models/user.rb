112bc3d3dd26105f78acf1c1cdea5204106333ab
Lisa Nguyen <email": null>
2017-01-17 16:50:07 -0800

    adding user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :listed_shoes, :foreign_key => :seller_id, :class_name => "Shoe"
+  has_many :purchases, :foreign_key => "purchaser_id"
+  has_many :purchased_shoes, :through => :purchases, :source => :shoe
+  has_many :sales, :through => :listed_shoes
+  has_many :customers, :through => :sales, :source => :purchaser
 end
