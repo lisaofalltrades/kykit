0aa68e1affc4f06a0671d7c557fdb4947670bcaa
Lisa Nguyen <email": null>
2017-01-17 17:13:11 -0800

    saving CreateShoes table migration DRAFT

--- db/migrate/20170117165455_create_shoes.rb
+++ db/migrate/20170117165455_create_shoes.rb
@@ -1,4 +1,12 @@
 class CreateShoes < ActiveRecord::Migration
   def change
+    create_table :shoes do |t|
+      t.string :title, null: false, limit: 64
+      t.decimal :price, null: false
+      t.text :description, null: false
+      t.string :condition, null: false
+
+      t.timestamp
+    end
   end
 end
