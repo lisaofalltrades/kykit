da6f98ddb9889cbdd4996f36d4dca70df506f1c1
Lisa Nguyen <email": null>
2017-01-20 10:50:03 -0800

    saving

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -7,7 +7,7 @@ get '/shoes/new' do
 end
 
 post '/shoes' do
-  @shoe = Shoe.new(title: params[:title], brand: params[:brand], size: params[:size], price: params[:price], condition: params[:condition], distance: params[:distance], description: params[:description], user: current_user)
+  @shoe = Shoe.new(params[:shoes])
     if @shoe.save
       redirect '/shoes'
     else
--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -2,5 +2,6 @@ class Shoe < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :seller, :class_name => "User"
   has_many :sales, :class_name => "Purchase"
-  has_many :purchasers, :through => :sales, :source => :purchaser
+  has_many :orders, :through => :sales
+  has_many :purchasers, :through => :orders, :source => :purchaser
 end
