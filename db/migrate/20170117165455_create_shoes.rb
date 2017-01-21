d3c796c6ec70e71d97e87f0717ad008685f056fe
Lisa Nguyen <email": null>
2017-01-20 18:05:04 -0800

    saving

--- db/migrate/20170117165455_create_shoes.rb
+++ db/migrate/20170117165455_create_shoes.rb
@@ -6,6 +6,7 @@ class CreateShoes < ActiveRecord::Migration
       t.integer :size, null: false
       t.float :price, null: false
       t.string :condition, null: false
+      t.integer :zip_code, null: false
       t.integer :distance, null: false
       t.text :description, null: false
       t.integer :seller_id
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 User.destroy_all
+Shoe.destroy_all
 
 ##### USER #####
 5.times do
@@ -10,13 +11,7 @@ User.destroy_all
   )
 end
 
-##### SHOES #####
-
-# shoe_list = [
-#   [ "Brand New Energy Cloud", "adidas", "9.5", "$36.88", "Never Worn", 50, "This is a description. lorem ipsum sdfiwe ksfjawer nasdkfjuwe ksdf" ],
-#   [ "Pretty New Rosherun", "Nike", "10", "$48.10", "Slightly Worn", 50, "This is a description. lorem ipsum sdfiwe ksfjawer nasdkfjuwe ksdf" ]
-# ]
-#
-# shoe_list.each do |title, brand, size, price, condition, miles_range, description|
-#   Shoes.create( title: title, brand: brand, size: size, price: price, condition: condition, miles_range: miles_range, description: description )
-# end
+Shoe.create(title: "Mishaped Uggs", brand: "Uggs" , size: 7 , price: 10, condition: "Tore Up" , zip_code: 95116 , distance: 2 , description: "super warm")
+Shoe.create(title: "5 Time Marathon Nikes", brand: "Nike" , size: 9 , price: 25, condition: "Tore Up" , zip_code: 95111 , distance: 5 , description: "they served me well")
+Shoe.create(title: "Boots with the Fur", brand: "Apple Bottom" , size: 5 , price: 15, condition: "Like New" , zip_code: 95116 , distance: 10 , description: "knee high")
+Shoe.create(title: "Rosche", brand: "adidas" , size: 7 , price: 10, condition: "Tore Up" , zip_code: 95116 , distance: 2 , description: "super warm")
